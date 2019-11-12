4e0a5d621b0e5b14687074668ccd1a5abd73e655
Shubham Srivastava <smjackson94@gmail.com>
2019-11-12 23:23:32 +0530

    learned js settimeout function

--- node_test.js
+++ node_test.js
@@ -1,14 +1,18 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// var http = require('http');
+// var url = require('url');
+// var fs = require('fs');
 
-// (function(){})
-http.createServer(function (req, res) {
-//  fs.readFile("html_test.html", function (err, data) {
-//    res.writeHead(200, {'Content-Type': 'text/html'});
-//    res.write(data);
-//    var q = url.parse(req.url, true).query;
-//    var txt = q.year + " " + q.month;
-//    res.end(txt);
-//  });  
-}).listen(8080);
+// // (function(){})
+// http.createServer(function (req, res) {
+// //  fs.readFile("html_test.html", function (err, data) {
+// //    res.writeHead(200, {'Content-Type': 'text/html'});
+// //    res.write(data);
+// //    var q = url.parse(req.url, true).query;
+// //    var txt = q.year + " " + q.month;
+// //    res.end(txt);
+// //  });  
+// }).listen(8080);
+setTimeout(function () {
+    console.log("timeout function");
+}, 5000);
+console.log("yeeeeeeeeee");
