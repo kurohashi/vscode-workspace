0a0f7035e88a3c4aab64b7490794176687f9484b
Shubham Srivastava <shubham@rytangle.com>
2019-11-18 16:15:21 +0530

    added sample angular tester

---
+++ angular-test/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { QuillModule } from 'ngx-quill';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    QuillModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
