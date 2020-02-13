35b437c0f0d4c9e20cf30a177c97b4fd7bcbef83
Shubham Srivastava <shubham@rytangle.com>
2020-02-13 18:54:22 +0530

    some change

--- a.c
+++ a.c
@@ -2,8 +2,20 @@
 #include<string.h>
 int main(int argc, char const *argv[])
 {    
-    char* a = "ABC";
-    char* b = "BC";
-    printf("%d %d %d %d %d", a, a + 1, a + 2, b, b + 1);  
+    int pens;
+    int packets_of_pencils;
+    int notebooks;
+    int cost_pen = 10;
+    int cost_packet_of_pencils = 50;
+    int cost_notebook = 50;
+    int price;
+    printf("Enter number of pens = ");
+    scanf("%d", &pens);
+    printf("Enter number of packets of pencils = ");
+    scanf("%d", &packets_of_pencils);
+    printf("Enter number of notebooks = ");
+    scanf("%d", &notebooks);
+    price = (pens * cost_pen) + (packets_of_pencils * cost_packet_of_pencils) + (notebooks * cost_notebook);
+    printf("Your total price is = %d", price);
     return 0;
 }
