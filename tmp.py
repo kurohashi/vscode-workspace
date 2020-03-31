5ae5de8ff32984358238b4a793fd8d3987610c97
Shubham Srivastava <smjackson94@gmail.com>
2020-03-31 09:53:32 +0530

    changed pytho

--- tmp.py
+++ tmp.py
@@ -2,25 +2,41 @@ n, m = input().split()
 n = int(n)
 m = int(m)
 grid = []
-for i in range(int(n)):
-    grid.append(input())
-result = [0, 0]
 for i in range(n):
-    for j in range(m):
-        if grid[i][j] == 'G':
-            y = 0
-            for k in range(1, min(i + 1, j + 1, n - i, m - j)):
-                if grid[i - k][j] != 'G':
-                    break
-                if grid[i][j - k] != 'G':
-                    break
-                if grid[i + k][j] != 'G':
-                    break
-                if grid[i][j + k] != 'G':
-                    break
-                y += 1
-            result.append(y)
-a = max(result)
-result.remove(a)
-a = (a * 4 + 1) * (max(result) * 4 + 1)
+    grid.append(list(input()))
+result = []
+x = 2
+while x:
+    tmpMax = 1
+    indMax = [-1, -1]
+    for i in range(n):
+        for j in range(m):
+            if grid[i][j] == 'G':
+                y = 1
+                for k in range(1, min(i + 1, j + 1, n - i, m - j)):
+                    if grid[i - k][j] != 'G':
+                        break
+                    if grid[i][j - k] != 'G':
+                        break
+                    if grid[i + k][j] != 'G':
+                        break
+                    if grid[i][j + k] != 'G':
+                        break
+                    y += 4
+                if y > tmpMax:
+                    tmpMax = y
+                    indMax = [i, j]
+    result.append(tmpMax)
+    grid[indMax[0]][indMax[1]] = 'B'
+    tmpMax -= 1
+    tmpI = 1
+    while tmpMax:
+        grid[indMax[0]][indMax[1] - tmpI] = 'B'
+        grid[indMax[0]][indMax[1] + tmpI] = 'B'
+        grid[indMax[0] - tmpI][indMax[1]] = 'B'
+        grid[indMax[0] + tmpI][indMax[1]] = 'B'
+        tmpMax -= 4
+        tmpI += 1
+    x -= 1
+a = result[0] * result[1]
 print(a)
