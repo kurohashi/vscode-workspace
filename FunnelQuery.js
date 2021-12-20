959d15c0e8227f4e4c144b45cdb6bc0a5da0d142
Shubham Srivastava <smjackson94@gmail.com>
2021-12-20 16:04:33 +0530

    practive sub domain cookies

--- FunnelQuery.js
+++ FunnelQuery.js
@@ -1,6 +1,6 @@
 // var streamcount = require('streamcount');
 // var client = require("redis").createClient();
- 
+
 // // Create a stream counter to track unique visitors with a 1% margin of error.
 // var uniques = streamcount.createUniquesCounter(0.01);
 
@@ -22,11 +22,11 @@
 // //         console.log(err, reply);
 // //     });
 // // }).catch(err => console.error(err));
- 
- 
+
+
 // // Create a stream counter to track the top 3 pages viewed on our site.
 // // var pageCounts = streamcount.createViewsCounter(3);
- 
+
 // // // Add some observations
 // // let arr = [];
 // // pageCounts.increment('/');
@@ -41,20 +41,62 @@
 // // pageCounts.increment('/product1');
 // // pageCounts.increment('/');
 // // pageCounts.increment('/product1');
- 
+
 // // // Prints [ [ 4, '/' ], [ 3, '/product1' ], [ 2, '/about' ] ]
 // // console.log(pageCounts.getTopK());
 
-let { JSDOM } = require("jsdom");
+// let { JSDOM } = require("jsdom");
 
-let a = '<a id="js-go-to-sandbox" class="go-to-sandbox js-navigate " href="/sandbox/1621016/details/">\
-Open in Sandbox &gt;\
-</a>\
-';
+// let a = '<a id="js-go-to-sandbox" class="go-to-sandbox js-navigate " href="/sandbox/1621016/details/">\
+// Open in Sandbox &gt;\
+// </a>\
+// ';
 
-let tag = a.substr(1, a.indexOf(" ") - 1);
-console.log(tag);
+// let tag = a.substr(1, a.indexOf(" ") - 1);
+// console.log(tag);
 
-let dom = new JSDOM(a);
-let element = dom.window.document.getElementsByTagName(tag)[0];
-console.log(element.className, element.id);
+// let dom = new JSDOM(a);
+// let element = dom.window.document.getElementsByTagName(tag)[0];
+// console.log(JSON.stringify(dom.window.location));
+
+// let url = new URL("https://fibotalk.com/?a=1&a=2#test");
+// console.log(url.searchParams.get("a"))
+
+var info = console.log;
+
+var console = {
+    log: function log(message) {
+        const callerInfo = getFileName(log.caller.name);
+        info(`${new Date()} <${arguments.callee.name}> ${callerInfo.filename} (${log.caller.name}) ${message}`);
+    },
+};
+
+function getFileName(caller) {
+    const STACK_FUNC_NAME = new RegExp(/at\s+((\S+)\s)?\((\S+):(\d+):(\d+)\)/);
+    let err = new Error();
+
+    Error.captureStackTrace(err);
+
+    let stacks = err.stack.split('\n').slice(1);
+
+    let callerInfo = null;
+    for (let i = 0; i < stacks.length; i++) {
+        callerInfo = STACK_FUNC_NAME.exec(stacks[i]);
+
+        if (callerInfo[2] === caller) {
+            return {
+                filename: callerInfo[3],
+                line: callerInfo[4],
+                column: callerInfo[5],
+            };
+        }
+    }
+
+    return null;
+}
+
+function iWantToLog() {
+    console.log('Testing my log');
+}
+
+iWantToLog();
---
+++ jslearner.js
@@ -0,0 +1,39 @@
+var host = location.host.split(".");
+if (host.length == 1) {
+    // for localhost
+    document.cookie = cookie;
+} else {
+    document.cookie = "a=1;domain=." + host.slice(1).join(".");
+    var val = getCookie(cname);
+    if (!val || val != cvalue)
+        document.cookie = "a=1;domain=." + location.host;
+}
+
+function getCookie(cname) {
+    try {
+        var cookie;
+        var name = cookieSettings + "=";
+        var decodedCookie = document.cookie;
+        console.log(decodedCookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                cookie = c.substring(name.length, c.length);
+                break;
+            }
+        }
+        cookie = window.atob(cookie);
+        cookie = JSON.parse(cookie);
+        if (!cname)
+            return cookie;
+        if (cookie[cname])
+            return cookie[cname];
+    } catch (error) {
+        console.log(error);
+    }
+    return null;
+}
