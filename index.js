099e8bcd52a1582202694a40ae1af74fd9e95492
Shubham Srivastava <smjackson94@gmail.com>
2022-06-02 16:42:47 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const client = require("redis").createClient({
-    host: "fibo-mq-001.sw5shl.0001.usw2.cache.amazonaws.com",
-    port: "6379"
+    url: "fibo-mq-001.sw5shl.0001.usw2.cache.amazonaws.com:6379",
 });
 
 start().catch(err => {
