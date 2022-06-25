c60db433a7d5660329bb04a7e06466053a86a1c3
Shubham Srivastava <smjackson94@gmail.com>
2022-06-25 16:36:39 +0530

    puppeteer example

--- index.js
+++ index.js
@@ -1,87 +1,89 @@
 const conf = {
     port: 10000,
-    kafka: {
-        brokers: ['b-1.fibotalk.aeljif.c8.kafka.us-west-2.amazonaws.com:9092', 'b-2.fibotalk.aeljif.c8.kafka.us-west-2.amazonaws.com:9092', 'b-3.fibotalk.aeljif.c8.kafka.us-west-2.amazonaws.com:9092'],
-        groupId: "test",
-        topic: "test",
-    }
 };
 
-const request = require("request");
-const bodyParser = require("body-parser");
+const { JSDOM } = require("jsdom");
+const puppeteer = require("puppeteer");
 
+const bodyParser = require("body-parser");
 const app = require("express")();
 app.use(bodyParser.json()); //supprts JSON encoded bodies
 app.use(bodyParser.text()); //for navigator.sendBeacon api call
 app.use(bodyParser.urlencoded({ extended: true })); //supports URL encoded bodies
-app.post("/publish", publish);
+app.post("/get/reviews", getReviews);
 app.listen(conf.port);
 
-const { Kafka } = require("kafkajs");
-const kafka = new Kafka({
-    clientId: 'fibotalk',
-    brokers: conf.kafka.brokers
-});
-const producer = kafka.producer({});
-const consumer = kafka.consumer({ groupId: conf.kafka.groupId, sessionTimeout: 30000, heartbeatInterval: 3000 });
-
-start().then(resp => console.log("Server setup complete on 10000", resp)).catch(err => console.error(err));
-
+console.log("Service started on port:", conf.port);
+console.log("API details:\n");
+console.log(`URL: http://localhost:${conf.port}/get/reviews\nMETHOD: post\nBODY: { url: "url to be crawled" }]\n\n`);
 
 /**
- * Start the execution
- */
-async function start() {
-    await producer.connect();
-    await consumer.subscribe({ topic: conf.kafka.topic });
-    await consumer.run({
-        autoCommitThreshold: 1,
-        eachMessage: eachMessage,
-        partitionsConsumedConcurrently: 1,
-    });
-}
-
-/**
- * Each event consumer
- * @param {*} resp 
- */
-async function eachMessage(resp) {
-    let event = JSON.parse(resp.message.value.toString());
-    let gid = resp.message.key.toString();
-
-    return new Promise(function (resolve, reject) {
-        request({
-            method: "POST",
-            url: "http://localhost:3000/monitor",
-            json: event,
-            headers: { gid: gid },
-        }, function (err, resp, body) {
-            if (err)
-                return reject(err);
-            console.log(body);
-            resolve(body);
-        });
-    });
-}
-
-/**
- * Publish event to kafka
+ * Get reviews
  * @param {*} req 
  * @param {*} res 
  */
-function publish(req, res) {
-    if (!req.headers.gid)
-        return res.status(400).json();
-    producer.send({
-        topic: conf.kafka.topic,
-        messages: [{
-            value: JSON.stringify(req.body),
-            key: req.headers.gid,
-        }],
-    }).then(resp => {
-        res.status(200).json({ message: "Success" });
+function getReviews(req, res) {
+    if (!req.body.url)
+        return res.status(400).json({ msg: "url not provided" });
+    crawlReviews(req.body.url).then(resp => {
+        console.log(JSON.stringify(resp));
+        res.status(200).json({
+            data: resp,
+            msg: "success",
+        });
     }).catch(err => {
         console.error(err);
-        res.status(500).json(err);
+        res.status(400).json({ msg: "wrong url" });
     });
+}
+
+/**
+ * Crawl reviews from the url
+ * @param {*} url 
+ */
+async function crawlReviews(url) {
+    let dom = new JSDOM(await loadUrl(url));
+    let div = dom.window.document.getElementById("customerReviews");
+    let result = [];
+    for (let node of div.children) {
+        if (!node.id == "customerReviews")
+            continue;
+        let obj = {};
+        for (let datNode of node.children[0].children) {
+            if (datNode.className.includes("leftCol")) {
+                for (let rNode of datNode.children) {
+                    if (rNode.className.includes("itemReview")) {
+                        obj.rating = rNode.children[1].textContent.trim();
+                    } else if (rNode.className.includes("reviewer")) {
+                        obj.name = rNode.children[1].textContent.trim();
+                        obj.date = rNode.children[3].textContent.trim();
+                    }
+                }
+            } else if (datNode.className.includes("rightCol")) {
+                obj.comment = datNode.textContent.trim();
+            }
+        }
+        if (Object.keys(obj).length)
+            result.push(obj);
+    }
+    return result;
+}
+
+/**
+ * Load the html url
+ * @param {*} url 
+ */
+async function loadUrl(url) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ["--disable-setuid-sandbox"],
+        ignoreHTTPSErrors: true,
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector("#customerReviews");
+    const bodyHandle = await page.$('body');
+    const html = await page.evaluate(body => body.innerHTML, bodyHandle);
+    await bodyHandle.dispose();
+    return html;
 }
