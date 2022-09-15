9abe09700f326678ad5100b48279a94f17fe7e6a
Shubham Srivastava <smjackson94@gmail.com>
2022-09-15 09:12:04 +0530

    groupId changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var conf = {
     kafka: {
         brokers: ['b-3.kaliper.b30u2l.c8.kafka.us-west-2.amazonaws.com:9092', 'b-2.kaliper.b30u2l.c8.kafka.us-west-2.amazonaws.com:9092', 'b-1.kaliper.b30u2l.c8.kafka.us-west-2.amazonaws.com:9092'],
-        groupId: "dev-kaliper-transformations-*-",
+        groupId: "kaliper-transformations",
         clientId: "kaliper",
         topic: (key => `kaliper-${key}`),
         forwardingTopic: `kaliper-forwards`,
@@ -19,10 +19,12 @@ const kafka = new Kafka({
     brokers: conf.kafka.brokers
 });
 const consumer = kafka.consumer({ groupId: conf.kafka.groupId, sessionTimeout: 30000, heartbeatInterval: 3000 });
+const producer = kafka.producer({});
 
 start().catch(err => console.error(err));
 
 async function start() {
+    await producer.connect();
     await consumer.subscribe({ topic: conf.kafka.forwardingTopic });
     await consumer.run({
         autoCommitThreshold: 1,
@@ -33,5 +35,21 @@ async function start() {
 
 
 async function eachMessage(resp) {
-    console.log(resp.message.value.toString());
+    let message = resp.message.value.toString();
+    console.log(message);
+    let data = JSON.parse(message);
+    if (isNaN(data.count))
+        data.count = 0;
+    if (data.count >= 3)
+        return;
+    try {
+        
+    } catch (error) {
+        data.count++;
+        await producer.send({
+            topic: conf.kafka.forwardingTopic,
+            messages: [{ value: JSON.stringify(data) }],
+        });
+        return true;
+    }
 }
