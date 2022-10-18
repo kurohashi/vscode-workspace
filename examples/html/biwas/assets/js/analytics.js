7653707c0440f390c51b3e31e3c97768d13e1258
Shubham Srivastava <smjackson94@gmail.com>
2022-10-18 16:23:00 +0530

    comment

--- examples/html/biwas/assets/js/analytics.js
+++ examples/html/biwas/assets/js/analytics.js
@@ -159,11 +159,11 @@ function isTabHidden() {
 (function () {
     var origOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function () {
-        console.log('request started!');
+        // console.log('request started!');
         this.addEventListener('load', function (e) {
-            console.log('request completed!', e, this.responseType, this.responseURL);
-            console.log(this.readyState); //will always be 4 (ajax is completed successfully)
-            console.log(this.responseText); //whatever the response was
+            console.log('request completed!', this.responseURL);
+            // console.log(this.readyState); //will always be 4 (ajax is completed successfully)
+            // console.log(this.responseText); //whatever the response was
         });
         origOpen.apply(this, arguments);
     };
