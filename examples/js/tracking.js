f6db58cffa2398908d358f9c500b8c9aefbd5f52
Shubham Srivastava <smjackson94@gmail.com>
2022-10-25 19:35:35 +0530

    testing

--- examples/js/tracking.js
+++ examples/js/tracking.js
@@ -1,11 +1,39 @@
+// (function () {
+//     var origOpen = XMLHttpRequest.prototype.open;
+//     XMLHttpRequest.prototype.open = function () {
+//         this.addEventListener('load', function (e) {
+//             console.log('Tracked by XHR', this);
+//         });
+//         origOpen.apply(this, arguments);
+//     };
+// })();
+
 (function () {
-    var origOpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function () {
-        this.addEventListener('load', function (e) {
-            console.log('Tracked by XHR', this);
-        });
-        origOpen.apply(this, arguments);
-    };
+    var open = window.XMLHttpRequest.prototype.open,
+        send = window.XMLHttpRequest.prototype.send;
+
+    function openReplacement(method, url, async, user, password) {
+        this._url = url;
+        return open.apply(this, arguments);
+    }
+
+    function sendReplacement(data) {
+        if (this.onreadystatechange) {
+            this._onreadystatechange = this.onreadystatechange;
+        }
+        console.log('Tracked by XHR', this);
+        this.onreadystatechange = onReadyStateChangeReplacement;
+        return send.apply(this, arguments);
+    }
+
+    function onReadyStateChangeReplacement() {
+        if (this._onreadystatechange) {
+            return this._onreadystatechange.apply(this, arguments);
+        }
+    }
+
+    window.XMLHttpRequest.prototype.open = openReplacement;
+    window.XMLHttpRequest.prototype.send = sendReplacement;
 })();
 
 (function () {
