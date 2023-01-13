0eb39a31872547409fff0428eab80b43d6a2cc23
Shubham Srivastava <smjackson94@gmail.com>
2023-01-13 18:21:02 +0530

    rudderstack added to analytics set

--- index.js
+++ index.js
@@ -1,101 +1,35 @@
 /**
- * @param {number} n
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
  */
-var ExamRoom = function(n) {
-    this.total = n;
-    this.room = [];
-};
-
 /**
- * @return {number}
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
  */
-ExamRoom.prototype.seat = function() {
-    switch (this.room.length) {
-        case 0:
-            this.room.push(0);
-            return 0;
-        case 1:
-            return get1SizeSeat(this.room, this.total);
-        default:
-            let newSeat = getNewSeatIndex(this.room);
-            this.room.splice(newSeat.index, 0, newSeat.value);
-            return newSeat.value;
-    }
-};
-
-/**
- * 
- * @param {Array} room 
- * @param {number} total 
- */
-function get1SizeSeat(room, total) {
-    switch (room[0]) {
-        case 0:
-            room.push(total - 1);
-            return total - 1;
-        case total - 1:
-            room.unshift(0);
-            return 0;
-        default:
-            let firstHalf = Math.floor(this.room[0] / 2), secondHalf = Math.floor((this.total - 1 - this.room[0]) / 2) - this.room[0];
-            if (this.room[0] < (this.total - 1 - this.room[0])) {
-                this.room.push(this.total - 1);
-                return this.total - 1;
-            } else {
-
+var deleteNode = function(root, key) {
+    if (root.val == key) {
+        if (!root.left) {
+            return root.right;
+        } else if (!root.right) {
+            return root.left;
+        } else {
+            let foo = root;
+            while (foo.right) {
+                foo.val = foo.right.val;
+                if (!foo.right.right)
+                    foo.right = null;
+                foo = foo.right;
             }
-    }
-}
-
-/**
- * 
- * @param {Array} room 
- */
-function getNewSeatIndex(room) {
-    let maxDiff = Math.floor((room[1] + room[0]) / 2) - room[0], maxInd = [0, 1];
-    for (let i = 2; i < room.length; i++) {
-        let fooDiff = Math.floor((room[i] + room[i - 1]) / 2) - room[i - 1];
-        if (fooDiff > maxDiff) {
-            maxDiff = fooDiff;
-            maxInd = [i - 1, i];
         }
+    } else if (root.val > key && root.left) {
+        root.left = deleteNode(root.left, key);
+    } else if (root.right) {
+        root.right = deleteNode(root.right, key);
     }
-    return { index: maxInd[1], value: Math.floor((room[maxInd[0]] + room[maxInd[1]]) / 2) };
+    return root;
 }
-
-/** 
- * @param {number} p
- * @return {void}
- */
-ExamRoom.prototype.leave = function(p) {
-    let index = binSearch(this.room, p);
-    if (index < 0)
-        return;
-    this.room.splice(index, 1);
-};
-
-/**
- * 
- * @param {Array} arr 
- * @param {number} elem 
- */
-function binSearch(arr, elem) {
-    let min = 0, max = arr.length - 1;
-    while (min <= max) {
-        let mid = Math.floor((max + min) / 2);
-        if (arr[mid] == elem)
-            return mid;
-        else if (elem < arr[mid])
-            max = mid - 1;
-        else
-            min = mid + 1;
-    }
-    return -1;
-}
-
-/** 
- * Your ExamRoom object will be instantiated and called as such:
- * var obj = new ExamRoom(n)
- * var param_1 = obj.seat()
- * obj.leave(p)
- */
