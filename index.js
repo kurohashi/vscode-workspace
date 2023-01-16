aa07f1983f373d7519c70af020e7ede7f0359d32
Shubham Srivastava <smjackson94@gmail.com>
2023-01-16 22:03:23 +0530

    mid solving

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number[][]} rects
  */
-/**
- * @param {TreeNode} root
- * @param {number} key
- * @return {TreeNode}
- */
-var deleteNode = function(root, key) {
-    if (root.val == key) {
-        if (!root.left) {
-            return root.right;
-        } else if (!root.right) {
-            return root.left;
-        } else {
-            let foo = root;
-            while (foo.right) {
-                foo.val = foo.right.val;
-                if (!foo.right.right)
-                    foo.right = null;
-                foo = foo.right;
-            }
-        }
-    } else if (root.val > key && root.left) {
-        root.left = deleteNode(root.left, key);
-    } else if (root.right) {
-        root.right = deleteNode(root.right, key);
+var Solution = function(rects) {
+    this.rects = rects;
+    this.weights = [];
+    for (let r of rects) {
+        
     }
-    return root;
-}
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.pick = function() {
+    
+};
+
+/** 
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(rects)
+ * var param_1 = obj.pick()
+ */
