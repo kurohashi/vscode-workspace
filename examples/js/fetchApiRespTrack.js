dac5dc04ffcb165f3eb1ea5bc2dddb129397c885
Shubham Srivastava <smjackson94@gmail.com>
2023-04-05 11:09:47 +0530

    fix

--- examples/js/fetchApiRespTrack.js
+++ examples/js/fetchApiRespTrack.js
@@ -1,8 +1,13 @@
-var __gtm__originalFetchApi__ = window.fetch;
+const __gtm__originalFetchApi__ = window.fetch;
 
 window.fetch = async function(...args) {
     var response = await __gtm__originalFetchApi__(...args);
-    var resp = await response.json();
-    console.log("fetch response tracker", resp);
+    var url = response.url;
+    try {
+        var resp = await response.json();
+        console.log("fetch response tracker", url, resp);
+    } catch (error) {
+        console.log(error, url, args);
+    }
     return response;
 }
